99b423cbdb692e837086eb52963ce8567b0d31b0
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-10-23 18:47:45 +0530

    group chat

--- backend/src/utils/db-connection.js
+++ backend/src/utils/db-connection.js
@@ -2,7 +2,10 @@ const {Sequelize}=require('sequelize')
 
 const sequelize=new Sequelize(process.env.DB_NAME,process.env.USER_NAME,process.env.DB_PASS,{
     host:process.env.HOST,
-    dialect:"mysql"
+     dialect: "postgres",
+    logging: false,
+    dialectOptions: process.env.DB_SSL === "true" ? { ssl: { rejectUnauthorized: false } } : {}
+  
 })
 
 module.exports = sequelize;
